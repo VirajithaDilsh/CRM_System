9debb760a51051d566dbd77ad780ef19b54f611c
Virajitha Dilshan <virajitha.Jayathilaka@gmail.com>
2026-05-05 16:01:23 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+require("dotenv").config();
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CRM Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+  
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+  
